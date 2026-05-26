6299ddbc9ebb9e3d4039fd71f395b93cce36bf9a
Michael Librizzi <mlibrizzi@gmail.com>
2026-05-26 14:45:09 -0700

    fix: try WILL5000 series ID for Wilshire 5000

--- api/fred.js
+++ api/fred.js
@@ -82,7 +82,7 @@ fetchSeries('CES0500000003'), // Avg hourly earnings
     fetchSeries('VIXCLS'),       // VIX volatility index
     fetchSeries('T5YIE'),         // 5Y TIPS breakeven inflation rate
     fetchSeries('T10YIE'),        // 10Y TIPS breakeven inflation rate
-    fetchSeries('WILL5000PR', 8),     // Wilshire 5000 price return index (active)
+    fetchSeries('WILL5000', 8),        // Wilshire 5000 total return index
     fetchSeries('GDP', 8),         // Nominal GDP (quarterly)
 ]);
 
